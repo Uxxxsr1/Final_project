c708fd9c78c5782c723ea53b732a4006e932b0e1
banrig <shevczov03@bk.ru>
2026-04-02 14:25:53 +0500

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,1 +1,11 @@
-# db/__init__.py
+# Initilization file for Flask app and db
+
+from flask import Flask
+from .models import db
+from .config import Config
+
+def create_main_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    return app
